457d08f5d04e9e61bd545fc7705d2fca204c3858
Bryce Looyenga <blooyeng@gmail.com>
2020-10-12 13:14:39 -0600

    Completed Part 07-01 and changes to Sports Statistics

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -2,20 +2,68 @@
 import java.util.Scanner;
 
 public class LiquidContainers {
+    
+   
+    
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int contents = 0; 
+        int secondContents = 0; 
 
         while (true) {
-            System.out.print("> ");
-
-            String input = scan.nextLine();
+            
+            System.out.println("First: " + contents + "/" + "100");
+            System.out.println("Second: " + secondContents + "/" + "100");
+            
+            String [] word = new String[1]; 
+            String input = scan.nextLine(); 
             if (input.equals("quit")) {
-                break;
+                break; 
+            }
+            word = input.split(" "); 
+            int num = Integer.valueOf(word[1]); 
+            String selection = word[0].toLowerCase(); 
+            
+            if (selection.equals("add") && num > 0) {
+                if (contents + num <= 100) {
+              contents = contents + num; 
+                } else {
+                    contents = 100; 
+                }
+            }
+            
+            if (selection.equals("move") && num > 0) {
+                if (secondContents + num <= 100  && contents > 0) {
+                    //if (contents - num < 0) {
+                   //     secondContents = contents; 
+                    //}
+                    secondContents = secondContents + num; 
+                    if (contents - num > 0) {
+                    contents = contents - num;
+                    
+                    } else {
+                        secondContents = contents; 
+                        contents = 0; 
+                        
+                    }
+                    
+                } else {
+                    secondContents = 100; 
+                }
+            }
+            
+            if (selection.equals("remove") && num > 0) {
+                if (secondContents - num > 0) {
+                    secondContents = secondContents - num; 
+                } else {
+                    secondContents = 0; 
+                }
+            }
+             
             }
 
         }
     }
 
-}
+
