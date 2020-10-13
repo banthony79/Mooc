fb30b1bf515340fb4b7d5ad35e994a860eb935a2
Bryce Looyenga <blooyeng@gmail.com>
2020-10-12 18:43:12 -0600

    completed Parts 06-03 & 06-04

--- part06-Part06_04.PrintingACollection/src/main/java/Main.java
+++ part06-Part06_04.PrintingACollection/src/main/java/Main.java
@@ -3,25 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Try out your class here
-        SimpleCollection j = new SimpleCollection("characters");
-        System.out.println(j);        
+      
+SimpleCollection c = new SimpleCollection("test");
+
+c.add("Wolverine");
+
+        System.out.println(c);
         
-        System.out.println();
+        c.add("Professor X");
         
-        j.add("magneto");
-        System.out.println(j);
-        
-        System.out.println();
-        
-        j.add("mystique");
-        System.out.println(j);
-        
-        System.out.println();
-        
-        j.add("phoenix");
-        System.out.println(j);
-        
-        
+        System.out.println(c); 
+       
     }
 }
--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -19,4 +19,33 @@ public class SimpleCollection {
         return this.elements;
     }
     
+   public boolean overOne() {
+       if (this.elements.size() > 1) {
+           return true; 
+       }
+  return false;
+   }
+    
+    
+    
+    public String toString() {
+    String print = "";
+String element = "";
+int i = 0;
+if (this.elements.isEmpty()) {
+return print + "The collection " + this.name + " is empty.";
 }
+for (String string: elements) {
+element = element + string.trim() + "\n";
+i++;
+}
+if (i==1) {
+return print + "The collection " + this.name + " has " + i + " element:\n" + element.trim();
+}
+return print + "The collection " + this.name + " has " + i + " elements:\n" + element.trim();
+ 
+}
+}
+
+// return printOutput + "\n" +
+            //"on the ride:\n" + peopleOnRide;
